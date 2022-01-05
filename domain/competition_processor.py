d288e82f03343d42126d8fb985fe1253080e0271
CharlesDampeyrou <charles.dampeyrou@gmail.com>
2022-01-05 11:08:15 +0100

    Correction du cas ou aucune nouvelle compétition n'est à prendre en compte

--- domain/competition_processor.py
+++ domain/competition_processor.py
@@ -14,15 +14,18 @@ class CompetitionProcessor:
         self.db_service = database_service
         self.point_computer = point_computer
         self.logger = logging.getLogger("CompetitionProcessor")
-    
+
     def initialize_point_type(self, date, origin_points="scrapping"):
         self.logger.info("Starting the initialisation of the point type %s..." % self.point_computer.point_type)
         self.point_computer.initialize(date=date, origin_points=origin_points)
         self.logger.info("Initialisation of the point type %s finished" % self.point_computer.point_type)
-    
+
     def compute_point_type(self, starting_date):
         self.logger.info("Starting to cumpute the %s points..." % self.point_computer.point_type)
         competition_dates = self.db_service.get_competition_dates(starting_date)
+        if len(competition_dates) == 0:
+            self.logger.info("Pas de nouvelles compétitions pour lesquelles calculer des points")
+            return
         msg = "%i dates de compétitions sur lesquelles calculer les points, du %s au %s"
         self.logger.info(msg % (len(competition_dates),str(min(competition_dates)), str(max(competition_dates))))
         phases = ["qualif", "demi", "finale", ""]
@@ -36,7 +39,7 @@ class CompetitionProcessor:
                                                                 phase=phase,
                                                                 competition_date=competition_date,
                                                                 force_value_computing=True)
-        
+
     def update_point_type(self, default_starting_date=datetime(2014,1,1)):
         self.logger.info("Mise à jour des points %s" % self.point_computer.point_type)
         update_starting_date = self.db_service.get_last_points_date(self.point_computer.point_type)
@@ -44,5 +47,3 @@ class CompetitionProcessor:
             update_starting_date = default_starting_date
             self.initialize_point_type(update_starting_date)
         self.compute_point_type(update_starting_date)
-        
-               
