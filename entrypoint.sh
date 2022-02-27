fccb4ef6894d71b805aa82cb75c204bedf0f2b0b
Charles Dampeyrou <cdampeyrou@aquiladata.fr>
2022-02-27 21:38:46 +0100

    mise à jour des fichiers csv par reboot du serveur

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 echo "entering entrypoint.sh"
 
+# Mise à jour des fichiers CSV
+echo "Mise à jour des fichiers de course"
+python update_csv_files.py
+
 #mise à jour de la BDD
+echo "Mise à jour de la base de données"
 python points_updating.py
 
 #lancement du serveur
+echo "Lancement du serveur"
 uwsgi app.ini
